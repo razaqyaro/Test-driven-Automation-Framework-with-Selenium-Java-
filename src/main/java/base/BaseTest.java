2f715678d722dd21ac0bbd873ffdb2cf93367816
razaqyaro <razaqyaro@gmail.com>
2024-03-04 12:01:07 +0000

    fix the on-push action in the yaml file

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -10,15 +10,8 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
-
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
